832a027b904a607a2d01d67177fbdef23ff36197
mtalaj00 <56231164+mtalaj00@users.noreply.github.com>
2022-04-03 21:53:05 +0200

    Add project files.

---
+++ WebApplication6/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+    }
+}
